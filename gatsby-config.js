6592fada71ac457b97e935592254ed76b67a33f7
Eunjae Lee <karis612@gmail.com>
2019-09-21 20:21:23 +0200

    update style & add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,14 @@ module.exports = {
     author: `Eunjae Lee`,
     description: `Software Engineer @ Algolia`,
   },
-  plugins: [`gatsby-theme-mdx-blog`, `gatsby-theme-mdx-tailwind-blog`],
+  plugins: [
+    `gatsby-theme-mdx-blog`,
+    `gatsby-theme-mdx-tailwind-blog`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-148411881-1',
+      },
+    },
+  ],
 };
