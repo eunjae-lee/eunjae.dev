f686914be52b404e7fccef416535d71386a61442
Eunjae Lee <eunjae.lee@algolia.com>
2019-10-25 19:06:25 +0200

    update template

--- _templates/blog/new/index.js
+++ _templates/blog/new/index.js
@@ -1,35 +1,31 @@
 const slugify = require('slugify');
 
 module.exports = {
-  prompt: ({ prompter }) => {
-    return new Promise((resolve, reject) => {
-      prompter
-        .prompt([
-          {
-            type: 'input',
-            name: 'slug',
-            message: 'Slug?',
-          },
-          {
-            type: 'input',
-            name: 'title',
-            message: 'Title?',
-          },
-          {
-            type: 'input',
-            name: 'description',
-            message: 'Description?',
-          },
-        ])
-        .then(({ slug, title, description }) => {
-          resolve({
-            title,
-            date: new Date().toISOString(),
-            slug: slugify(slug, { remove: /[*+~.()'"!:@]/g, lower: true }),
-            title,
-            description,
-          });
-        });
-    });
+  prompt: async ({ prompter }) => {
+    const { slug, title, description } = await prompter.prompt([
+      {
+        type: 'input',
+        name: 'slug',
+        message: 'Slug?',
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Description?',
+      },
+    ]);
+
+    return {
+      title,
+      date: new Date().toISOString(),
+      slug: slugify(slug, { remove: /[*+~.()'"!:@]/g, lower: true }),
+      title,
+      description,
+    };
   },
 };
