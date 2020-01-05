1596b11ef6e4bc7417b95bc5d5104885a171e024
Eunjae Lee <eunjae.lee@algolia.com>
2020-01-05 22:48:07 +0900

    fix typos

--- src/components/actionLink.js
+++ src/components/actionLink.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default ({ children, href }) => (
   <a
     href={href}
-    className="border rounded border-green-700 px-8 py-4 bg-green-500 hover:bg-green-600 text-green-100 text-lg font-bold"
+    className="px-8 py-4 border rounded border-green-600 hover:border-green-700 bg-green-500 hover:bg-green-600 text-green-100 text-lg font-bold"
   >
     {children}
   </a>
